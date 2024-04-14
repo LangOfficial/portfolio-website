498ebb43c4c0321234a6dd10499a86734dd4bb1d
Langli <Langli@gmal.com>
2024-04-14 15:58:43 -0700

    changed files

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,3 @@
-// vite.config.js
-import { resolve } from 'path'
-import { defineConfig } from 'vite'
-
-export default defineConfig({
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        nested: resolve(__dirname, 'projects.html'),
-      },
-    },
-  },
-})
+export default {
+  base: "/portfolio-website/"
+}
