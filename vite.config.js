4890ede938da3e837e259377dd90c50e6e266367
Langli <Langli@gmal.com>
2024-04-14 22:48:31 -0700

    retry

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
+import { resolve } from 'path'
+
 export default {
   base: '/portfolio-website/',
   build: {
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        something: resolve(__dirname, 'projects.html')
+        something: resolve(__dirname, 'project.html')
       }
     }
   }
